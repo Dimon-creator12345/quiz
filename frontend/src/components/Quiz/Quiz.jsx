f911591b746fcbcfa98b7ea4fe82e747b8044f4e
Dimon Ryback <pokerserieskompani10@gmail.com>
2021-03-25 18:16:32 +0200

    update

--- frontend/src/components/Quiz/Quiz.jsx
+++ frontend/src/components/Quiz/Quiz.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
 import "./Quiz.css";
+import QuizResultLogic from "./QuizResultLogic";
 
 function Quiz({ props, set }) {
   let [counter, setCounter] = useState(1);
   let [access, setAccess] = useState(false);
   let [accessRenderResultQuiz, setaccessRenderResultQuiz] = useState(false);
+  let [counterCorrectAnswer, setCounterCorrectAnswer] = useState([]);
+  let [result, setResult] = useState("");
   let actualPage = props.pages[counter - 1];
+
+  function QuizResultLogic1(answer) {
+    let correctAnswer = counterCorrectAnswer.filter((item, index) => {
+      return item === true;
+    });
+    console.log(correctAnswer);
+    correctAnswer.length > props["all pages"] * 0.33
+      ? setResult(props["description your result"][0])
+      : correctAnswer.length > props["all pages"] * 0.66
+      ? setResult(props["description your result"][1])
+      : setResult(props["description your result"][2]);
+    setaccessRenderResultQuiz(true);
+  }
   return (
     <div className="container_quiz">
       <div className="wrapper_quiz">
@@ -49,8 +65,12 @@ function Quiz({ props, set }) {
                     onClick={() => {
                       if (counter < props["all pages"]) {
                         setCounter(counter + 1);
+                        counterCorrectAnswer.push(
+                          item === actualPage["correct answer"]
+                        );
+                        setCounterCorrectAnswer([...counterCorrectAnswer]);
                       } else {
-                        setaccessRenderResultQuiz(true);
+                        QuizResultLogic1();
                       }
                     }}
                     className="conteiner_question__answer_options"
@@ -64,19 +84,23 @@ function Quiz({ props, set }) {
             {accessRenderResultQuiz ? (
               <div className="quiz_result_wrapper">
                 <div className="quiz_result_wrapper__intro_wrapper">
-                  <p className="quiz_result_wrapper__intro_wrapper__category_test">Тесты · {props.category}</p>
-                  <h1 className="quiz_result_wrapper__intro_wrapper__title">{props.header}</h1>
+                  <p className="quiz_result_wrapper__intro_wrapper__category_test">
+                    Тесты · {props.category}
+                  </p>
+                  <h1 className="quiz_result_wrapper__intro_wrapper__title">
+                    {props.header}
+                  </h1>
                 </div>
-                <div className="quiz_result_wrapper__description" >
-                <h2 className="quiz_result_wrapper__description__title">Ваши результаты</h2>
-                {props["description your result"].map((item, index) => {
-                  return (
-                    <p key={index} className="quiz_result_wrapper__description__text">
-                      {item}
+                <div className="quiz_result_wrapper__description">
+                  <h2 className="quiz_result_wrapper__description__title">
+                    Ваши результаты
+                  </h2>
+                  {accessRenderResultQuiz ? (
+                    <p className="quiz_result_wrapper__description__text">
+                      {result}
                     </p>
-                  );
-                })}
-              </div>
+                  ) : undefined}
+                </div>
               </div>
             ) : undefined}
           </div>
---
+++ frontend/src/components/Quiz/QuizResultLogic.jsx
@@ -0,0 +1,6 @@
+
+function QuizResultLogic(){
+
+}
+
+export default QuizResultLogic;
